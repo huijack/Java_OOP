45f89aaa36ecda8d82975013202927b31efc8bb9
Huijack <jacklim2626@gmail.com>
2025-12-25 23:48:00 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Object = An entity that holds data (attributes)
+        //          and can perform actions (methods)
+        //          It is a reference data type
+    }
+}
